ef188b1a07e36693e281734c7718db2e3a842dd2
LGalushka <liliya2615@gmail.com>
2026-02-17 18:45:52 +0300

    feat: добавлен компонент MovieGrid для отображения сетки фильмов

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
 import { Layout } from './container/layout/Layout';
+import { MovieGrid } from './container/movie/MovieGrid';
+import type { Movie } from './types/movie.types';
+
+// временные данные для текста
+const testMovie: Movie[] = [
+  {
+    imdbID: '1',
+    Title: 'Inception',
+    Year: '2010',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
+  },
+  {
+    imdbID: '2',
+    Title: 'The Matrix',
+    Year: '1999',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
+  },
+  {
+    imdbID: '3',
+    Title: 'Interstellar',
+    Year: '2014',
+    Type: 'movie',
+    Poster:
+      'https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OExYLTJzNjktM3RkY0U0M2M2My1hN5lLmpgCg.jpg',
+  },
+];
 
 function App() {
-  const handlerSearch = (query: string) => {
-    console.log('Поиск', query);
-    //здесь будет API
-  };
   return (
-    <>
-      <Layout onSearch={handlerSearch}>
-        <h1 style={{ color: 'white' }}>Добро пожаловить!</h1>
-        <p style={{ color: 'gray' }}>Здесь будут фильмы...</p>
-      </Layout>
-    </>
+    <Layout onSearch={(query) => console.log('Поиск', query)}>
+      <MovieGrid movies={testMovie} />
+    </Layout>
   );
 }
 
---
+++ src/container/movie/MovieGrid/MovieGrid.tsx
@@ -0,0 +1,25 @@
+import type { Movie } from '../../../types/movie.types';
+import { MovieCard } from '../MovieCard';
+import styles from './MovieGrid.module.css';
+
+interface MovieGridProps {
+  movies: Movie[];
+}
+
+export const MovieGrid = ({ movies }: MovieGridProps) => {
+  if (!movies.length) {
+    return (
+      <div className={styles.empty}>
+        <p>🎬 Фильмы не найдены</p>
+        <p className={styles.emptySub}>Попробуйте изменить поисковый запрос</p>
+      </div>
+    );
+  }
+  return (
+    <div className={styles.grid}>
+      {movies.map((movie) => (
+        <MovieCard key={movie.imdbID} movie={movie} />
+      ))}
+    </div>
+  );
+};
