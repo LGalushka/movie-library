4b8e76d7add8685e99ec933db3183651b90ef5ac
LGalushka <liliya2615@gmail.com>
2026-02-17 15:30:40 +0300

    feat: Добавлен Header обновлены стили для Input, Button

--- src/container/ui/Input/Input.tsx
+++ src/container/ui/Input/Input.tsx
@@ -4,17 +4,19 @@ import styles from './Input.module.css';
 interface InputProps {
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
   placeholder?: string;
   type?: string;
 }
 
-export const Input = ({ value, onChange, placeholder, type }: InputProps) => {
+export const Input = ({ value, onChange, placeholder, type, onKeyDown }: InputProps) => {
   return (
     <input
       className={styles.input}
       type={type}
       value={value}
       onChange={onChange}
+      onKeyDown={onKeyDown}
       placeholder={placeholder}
     />
   );
